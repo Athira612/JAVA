81dad2920a302e8e71858f24554c5960426a9aa8
Athira612 <aathirapadapurackal612@github.com>
2021-05-28 21:41:53 +0530

    program

---
+++ sample prgm/add.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Add {
+
+    public static void main(String[] args) {
+        
+        int a, b, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        a = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        b = sc.nextInt();
+        
+        sc.close();
+        sum = a + b;
+        System.out.println("Sum of these numbers: "+sum);
+    }
+}
---
+++ sample prgm/cmdtest.java
@@ -0,0 +1,11 @@
+class Cmdtest
+{
+public static void main(String args[])
+{
+System.out.println(args[0]);  
+System.out.println(args[1]);
+System.out.println(args[2]);
+int sum=Integer.parseInt(args[0])+Integer.parseInt(args[1])+Integer.parseInt(args[2]);
+System.out.println("Sum of values :"+sum+"----");
+}
+}
