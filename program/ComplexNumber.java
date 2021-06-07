7f67c96b681920419d49e87f0288af4736612ff6
Athira612 <74651653+Athira612@users.noreply.github.com>
2021-06-07 14:50:11 +0530

    Add files via upload

--- program/ComplexNumber.java
+++ program/ComplexNumber.java
@@ -1,29 +1,30 @@
-public class ComplexNumber
-{
-   //for real and imaginary parts of complex numbers
-   double real, img;
-	
-   //constructor to initialize the complex number
-   ComplexNumber(double r, double i){
-	this.real = r;
-	this.img = i;
-   }
-	
-   public static ComplexNumber sum(ComplexNumber c1, ComplexNumber c2)
-   {
-	//creating a temporary complex number to hold the sum of two numbers
-        ComplexNumber temp = new ComplexNumber(0, 0);
+import java.util.Scanner;
+class ComplexNumber{
+     double real,img;
+   ComplexNumber(double r,double i){
+       this.real = r;
+       this.img = i;  
+     }
+   public static ComplexNumber sum(ComplexNumber c1, ComplexNumber c2){
+     ComplexNumber temp = new ComplexNumber(0,0);
+     temp.real = c1.real + c2.real;
+     temp.img = c1.img + c2.img;
+     return temp;
+    }
 
-        temp.real = c1.real + c2.real;
-        temp.img = c1.img + c2.img;
-        
-        //returning the output complex number
-        return temp;
-    }
-    public static void main(String args[]) {
-	ComplexNumber c1 = new ComplexNumber(5.5, 4);
-	ComplexNumber c2 = new ComplexNumber(1.2, 3.5);
-        ComplexNumber temp = sum(c1, c2);
-        System.out.printf("Sum is: "+ temp.real+" + "+ temp.img +"i");
-    }
+   public static void main(String args[]){
+       Scanner input = new Scanner(System.in);
+       System.out.println("Enter real part of first number:");
+       float n1 = input.nextFloat();
+       System.out.println("Enter imaginary part of first number:");
+       float n2 = input.nextFloat();
+       System.out.println("Enter real part of Second number:");
+       float n3 = input.nextFloat();
+       System.out.println("Enter imaginary part of Second number:");
+       float n4 = input.nextFloat();
+       ComplexNumber c1 = new ComplexNumber(n1,n2);
+       ComplexNumber c2 = new ComplexNumber(n3,n4);
+       ComplexNumber temp = sum(c1,c2);
+       System.out.println("Sum is :"+temp.real+ " + "+temp.img+" i" ); 
+  }
 }
